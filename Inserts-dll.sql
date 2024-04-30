0f47967967d5ac2c7dc85a6fa23c7ea7df6d63c5
Fernando Arias <82820449+fernAP16@users.noreply.github.com>
2024-04-30 14:44:28 -0500

    Initial commit

---
+++ Inserts-dll.sql
@@ -0,0 +1,93 @@
+-- sd_usuario
+INSERT INTO sd_usuario (nombres, primer_apellido, segundo_apellido, dni, fecha_nacimiento, email, peso, activo, fecha_registro, usuario_registro) VALUES ('JORGE SANDRO','TARAZONA','MUNAYCO','48641955','1980-10-01','jorge.tarazona@gmail.com',78.2,1,sysdate(),'admin');
+INSERT INTO sd_usuario (nombres, primer_apellido, segundo_apellido, dni, fecha_nacimiento, email, peso, activo, fecha_registro, usuario_registro) VALUES ('JUAN CARLOS','BAUTISTA','ENRIQUEZ','15879523','1982-08-30','juan.bautista@gmail.com',64.9,1,sysdate(),'admin');
+INSERT INTO sd_usuario (nombres, primer_apellido, segundo_apellido, dni, fecha_nacimiento, email, peso, activo, fecha_registro, usuario_registro) VALUES ('ABEL ROBERTO','DIAZ','FERNANDEZ','08512548','1979-07-15','abel.diaz@gmail.com',75.2,1,sysdate(),'admin');
+INSERT INTO sd_usuario (nombres, primer_apellido, segundo_apellido, dni, fecha_nacimiento, email, peso, activo, fecha_registro, usuario_registro) VALUES ('EMANUEL ','VILLAFUERTE','DOMINGUEZ','12012589','1981-08-04','emanuel.villafuerte@gmail.com',73.6,1,sysdate(),'admin');
+INSERT INTO sd_usuario (nombres, primer_apellido, segundo_apellido, dni, fecha_nacimiento, email, peso, activo, fecha_registro, usuario_registro) VALUES ('CLAUDIO JORGE','AGUIRRE','HERRERA','23567845','1978-01-05','claudio.aguirre@gmail.com',69.8,1,sysdate(),'admin');
+INSERT INTO sd_usuario (nombres, primer_apellido, segundo_apellido, dni, fecha_nacimiento, email, peso, activo, fecha_registro, usuario_registro) VALUES ('PEDRO MARIO','ALVAREZ','GONZALEZ','12547895','1985-03-29','pedro.gonzalez@gmail.com',75.4,1,sysdate(),'admin');
+INSERT INTO sd_usuario (nombres, primer_apellido, segundo_apellido, dni, fecha_nacimiento, email, peso, activo, fecha_registro, usuario_registro) VALUES ('CESAR FERNANDO','PEREZ','TORRES','24516874','1984-05-12','cesar.torres@gmail.com',76.5,1,sysdate(),'admin');
+INSERT INTO sd_usuario (nombres, primer_apellido, segundo_apellido, dni, fecha_nacimiento, email, peso, activo, fecha_registro, usuario_registro) VALUES ('VICTOR SERGIO','ROMERO','ACOSTA','30214587','1975-04-17','victor.acosta@gmail.com',78.9,1,sysdate(),'admin');
+
+-- sd_conductor
+INSERT INTO sd_conductor(id_usuario, fecha_venc_licencia, clave_digital, activo, fecha_registro, usuario_registro) VALUES (1,'2024-10-04','8421',1,sysdate(),'admin');
+INSERT INTO sd_conductor(id_usuario, fecha_venc_licencia, clave_digital, activo, fecha_registro, usuario_registro) VALUES (2,'2025-03-06','1269',1,sysdate(),'admin');
+INSERT INTO sd_conductor(id_usuario, fecha_venc_licencia, clave_digital, activo, fecha_registro, usuario_registro) VALUES (3,'2024-08-14','4589',1,sysdate(),'admin');
+INSERT INTO sd_conductor(id_usuario, fecha_venc_licencia, clave_digital, activo, fecha_registro, usuario_registro) VALUES (4,'2024-01-28','3586',1,sysdate(),'admin');
+
+-- sd_revisor
+INSERT INTO sd_revisor(id_usuario, contrasena, activo, fecha_registro, usuario_registro) VALUES (5,'r8t4b45sf',1,sysdate(),'admin');
+INSERT INTO sd_revisor(id_usuario, contrasena, activo, fecha_registro, usuario_registro) VALUES (6,'rgr1d5a8f',1,sysdate(),'admin');
+INSERT INTO sd_revisor(id_usuario, contrasena, activo, fecha_registro, usuario_registro) VALUES (7,'g5ds6dfe4',1,sysdate(),'admin');
+INSERT INTO sd_revisor(id_usuario, contrasena, activo, fecha_registro, usuario_registro) VALUES (8,'2rf1s8f4s6',1,sysdate(),'admin');
+
+-- sd_revisor
+INSERT INTO sd_producto(nombre, activo, fecha_registro, usuario_registro) VALUES ('Cemento',1,sysdate(),'admin');
+INSERT INTO sd_producto(nombre, activo, fecha_registro, usuario_registro) VALUES ('Ladrillo',1,sysdate(),'admin');
+INSERT INTO sd_producto(nombre, activo, fecha_registro, usuario_registro) VALUES ('Pallets',1,sysdate(),'admin');
+
+-- sd_marca
+INSERT INTO sd_marca(nombre, activo, fecha_registro, usuario_registro) VALUES ('APU',1,sysdate(),'admin');
+INSERT INTO sd_marca(nombre, activo, fecha_registro, usuario_registro) VALUES ('SOL',1,sysdate(),'admin');
+INSERT INTO sd_marca(nombre, activo, fecha_registro, usuario_registro) VALUES ('PIRAMIDE',1,sysdate(),'admin');
+INSERT INTO sd_marca(nombre, activo, fecha_registro, usuario_registro) VALUES ('BASA',1,sysdate(),'admin');
+INSERT INTO sd_marca(nombre, activo, fecha_registro, usuario_registro) VALUES ('LARK',1,sysdate(),'admin');
+INSERT INTO sd_marca(nombre, activo, fecha_registro, usuario_registro) VALUES ('CHEP',1,sysdate(),'admin');
+
+-- sd_unidad
+INSERT INTO sd_unidad(nombre, activo, fecha_registro, usuario_registro) VALUES ('bolsas de 42.5 kg',1,sysdate(),'admin');
+INSERT INTO sd_unidad(nombre, activo, fecha_registro, usuario_registro) VALUES ('bolsas de 52.5 kg',1,sysdate(),'admin');
+INSERT INTO sd_unidad(nombre, activo, fecha_registro, usuario_registro) VALUES ('ladrillos',1,sysdate(),'admin');
+INSERT INTO sd_unidad(nombre, activo, fecha_registro, usuario_registro) VALUES ('pallets',1,sysdate(),'admin');
+
+-- sd_producto_venta
+INSERT INTO sd_producto_venta (id_producto, id_marca, id_unidad, stock_actual, activo, fecha_registro, usuario_registro) VALUES (1,1,1,10000,1,sysdate(),'admin');
+INSERT INTO sd_producto_venta (id_producto, id_marca, id_unidad, stock_actual, activo, fecha_registro, usuario_registro) VALUES (1,2,2,15000,1,sysdate(),'admin');
+INSERT INTO sd_producto_venta (id_producto, id_marca, id_unidad, stock_actual, activo, fecha_registro, usuario_registro) VALUES (2,3,3,12000,1,sysdate(),'admin');
+INSERT INTO sd_producto_venta (id_producto, id_marca, id_unidad, stock_actual, activo, fecha_registro, usuario_registro) VALUES (2,5,3,18000,1,sysdate(),'admin');
+INSERT INTO sd_producto_venta (id_producto, id_marca, id_unidad, stock_actual, activo, fecha_registro, usuario_registro) VALUES (3,4,4,8000,1,sysdate(),'admin');
+INSERT INTO sd_producto_venta (id_producto, id_marca, id_unidad, stock_actual, activo, fecha_registro, usuario_registro) VALUES (3,6,4,13500,1,sysdate(),'admin');
+
+-- sd_tipo_vehiculo
+INSERT INTO sd_tipo_vehiculo (id_tipo_vehiculo, nombre) VALUES (1,'tracto');
+INSERT INTO sd_tipo_vehiculo (id_tipo_vehiculo, nombre) VALUES (2,'carreta');
+
+-- sd_vehiculo
+INSERT INTO sd_vehiculo(id_tipo_vehiculo, placa, largo, ancho, altura, peso, fecha_venc_circulacion, tiene_tarjeta_propiedad, fecha_venc_soat, activo, fecha_registro, usuario_registro) values (1,'A1A-489',6,2.4,3.7,8000,'2024-10-08',1,'2025-06-15',1,sysdate(),'admin');
+INSERT INTO sd_vehiculo(id_tipo_vehiculo, placa, largo, ancho, altura, peso, fecha_venc_circulacion, tiene_tarjeta_propiedad, fecha_venc_soat, activo, fecha_registro, usuario_registro) values (2,'O3B-678',12,2.4,2,5500,'2025-08-14',1,'2025-03-06',1,sysdate(),'admin');
+INSERT INTO sd_vehiculo(id_tipo_vehiculo, placa, largo, ancho, altura, peso, fecha_venc_circulacion, tiene_tarjeta_propiedad, fecha_venc_soat, activo, fecha_registro, usuario_registro) values (1,'A1A-348',6.2,2.5,3.8,7500,'2025-02-27',1,'2024-08-14',1,sysdate(),'admin');
+INSERT INTO sd_vehiculo(id_tipo_vehiculo, placa, largo, ancho, altura, peso, fecha_venc_circulacion, tiene_tarjeta_propiedad, fecha_venc_soat, activo, fecha_registro, usuario_registro) values (2,'O3B-148',13.5,2.5,2.2,6700,'2024-01-28',1,'2024-01-28',1,sysdate(),'admin');
+INSERT INTO sd_vehiculo(id_tipo_vehiculo, placa, largo, ancho, altura, peso, fecha_venc_circulacion, tiene_tarjeta_propiedad, fecha_venc_soat, activo, fecha_registro, usuario_registro) values (1,'A1A-782',6.5,2.6,4.0,7200,'2024-08-14',0,'2025-02-27',1,sysdate(),'admin');
+INSERT INTO sd_vehiculo(id_tipo_vehiculo, placa, largo, ancho, altura, peso, fecha_venc_circulacion, tiene_tarjeta_propiedad, fecha_venc_soat, activo, fecha_registro, usuario_registro) values (2,'O3B-258',12.5,2.6,2.4,8300,'2025-03-06',1,'2025-08-14',1,sysdate(),'admin');
+
+
+-- sd_cliente
+INSERT INTO sd_cliente (razon_social, ruc,activo, fecha_registro, usuario_registro) VALUES ('Ferreteria San Marcos','58945785216',1,sysdate(),'admin');
+INSERT INTO sd_cliente (razon_social, ruc,activo, fecha_registro, usuario_registro) VALUES ('Constructora San Martin','14895623487',1,sysdate(),'admin');
+INSERT INTO sd_cliente (razon_social, ruc,activo, fecha_registro, usuario_registro) VALUES ('Bechtel Perú','13462484613',1,sysdate(),'admin');
+INSERT INTO sd_cliente (razon_social, ruc,activo, fecha_registro, usuario_registro) VALUES ('Vinci SAC','48650480850',1,sysdate(),'admin');
+
+-- sd_sede_cliente
+INSERT INTO sd_sede_cliente(id_cliente, direccion, distrito, activo, fecha_registro, usuario_registro) VALUES (1,'Jr. Yavari 348','Villa el Salvador',1,sysdate(),'admin');
+INSERT INTO sd_sede_cliente(id_cliente, direccion, distrito, activo, fecha_registro, usuario_registro) VALUES (2,'Av. Izaguirre 4896','Independencia',1,sysdate(),'admin');
+INSERT INTO sd_sede_cliente(id_cliente, direccion, distrito, activo, fecha_registro, usuario_registro) VALUES (2,'Jr. Juliaca 789','Comas',1,sysdate(),'admin');
+INSERT INTO sd_sede_cliente(id_cliente, direccion, distrito, activo, fecha_registro, usuario_registro) VALUES (3,'Av. Los Alamos 3457','San Juan de Miraflores',1,sysdate(),'admin');
+INSERT INTO sd_sede_cliente(id_cliente, direccion, distrito, activo, fecha_registro, usuario_registro) VALUES (3,'Av. Jupiter 1278','Ate',1,sysdate(),'admin');
+INSERT INTO sd_sede_cliente(id_cliente, direccion, distrito, activo, fecha_registro, usuario_registro) VALUES (3,'Jr. Trueno 928','Ate',1,sysdate(),'admin');
+INSERT INTO sd_sede_cliente(id_cliente, direccion, distrito, activo, fecha_registro, usuario_registro) VALUES (4,'Av. Mariategui 2456','Villa Maria del Triunfo',1,sysdate(),'admin');
+INSERT INTO sd_sede_cliente(id_cliente, direccion, distrito, activo, fecha_registro, usuario_registro) VALUES (4,'Jr. Huiracocha 195','Independencia',1,sysdate(),'admin');
+
+-- sd_estado_orden
+INSERT INTO sd_estado_orden (nombre) VALUES ('registrada');
+INSERT INTO sd_estado_orden (nombre) VALUES ('despachada');
+
+-- sd_orden_recojo
+
+INSERT INTO sd_orden_recojo(id_conductor, id_sede_cliente, id_producto_venta, id_estado_orden, id_tracto, id_carreta, cantidad, activo, fecha_registro, usuario_registro) values (1,3,1,1,1,6,800,1,sysdate(),'admin');
+INSERT INTO sd_orden_recojo(id_conductor, id_sede_cliente, id_producto_venta, id_estado_orden, id_tracto, id_carreta, cantidad, activo, fecha_registro, usuario_registro) values (1,4,2,1,1,4,600,1,sysdate(),'admin');
+INSERT INTO sd_orden_recojo(id_conductor, id_sede_cliente, id_producto_venta, id_estado_orden, id_tracto, id_carreta, cantidad, activo, fecha_registro, usuario_registro) values (2,2,3,1,3,2,1500,1,sysdate(),'admin');
+INSERT INTO sd_orden_recojo(id_conductor, id_sede_cliente, id_producto_venta, id_estado_orden, id_tracto, id_carreta, cantidad, activo, fecha_registro, usuario_registro) values (3,5,4,1,5,2,1800,1,sysdate(),'admin');
+INSERT INTO sd_orden_recojo(id_conductor, id_sede_cliente, id_producto_venta, id_estado_orden, id_tracto, id_carreta, cantidad, activo, fecha_registro, usuario_registro) values (4,7,1,1,3,6,750,1,sysdate(),'admin');
+INSERT INTO sd_orden_recojo(id_conductor, id_sede_cliente, id_producto_venta, id_estado_orden, id_tracto, id_carreta, cantidad, activo, fecha_registro, usuario_registro) values (1,1,5,1,1,4,1200,1,sysdate(),'admin');
+INSERT INTO sd_orden_recojo(id_conductor, id_sede_cliente, id_producto_venta, id_estado_orden, id_tracto, id_carreta, cantidad, activo, fecha_registro, usuario_registro) values (2,8,6,1,5,6,1350,1,sysdate(),'admin');
+INSERT INTO sd_orden_recojo(id_conductor, id_sede_cliente, id_producto_venta, id_estado_orden, id_tracto, id_carreta, cantidad, activo, fecha_registro, usuario_registro) values (4,6,3,1,3,4,1750,1,sysdate(),'admin');
+
---
+++ Tables-ddl.sql
@@ -0,0 +1,484 @@
+DROP TABLE IF EXISTS sd_cola_pesaje;
+DROP TABLE IF EXISTS sd_tipo_pesaje;
+DROP TABLE IF EXISTS sd_zona_balanza;
+DROP TABLE IF EXISTS sd_cola_canal;
+DROP TABLE IF EXISTS sd_canal_carga;
+DROP TABLE IF EXISTS sd_despacho;
+DROP TABLE IF EXISTS sd_estado_despacho;
+DROP TABLE IF EXISTS sd_turno_incidencia;
+DROP TABLE IF EXISTS sd_incidencia;
+DROP TABLE IF EXISTS sd_turno_revision;
+DROP TABLE IF EXISTS sd_punto_control;
+DROP TABLE IF EXISTS sd_planta;
+DROP TABLE IF EXISTS sd_revisor;
+DROP TABLE IF EXISTS sd_orden_recojo;
+DROP TABLE IF EXISTS sd_estado_orden;
+DROP TABLE IF EXISTS sd_vehiculo;
+DROP TABLE IF EXISTS sd_tipo_vehiculo;
+DROP TABLE IF EXISTS sd_producto_venta;
+DROP TABLE IF EXISTS sd_producto;
+DROP TABLE IF EXISTS sd_marca;
+DROP TABLE IF EXISTS sd_unidad;
+DROP TABLE IF EXISTS sd_sede_cliente;
+DROP TABLE IF EXISTS sd_cliente;
+DROP TABLE IF EXISTS sd_conductor;
+DROP TABLE IF EXISTS sd_usuario;
+
+CREATE TABLE sd_usuario (
+    id_usuario INT NOT NULL AUTO_INCREMENT,
+    nombres VARCHAR(100) NOT NULL,
+    primer_apellido VARCHAR(100) NOT NULL,
+    segundo_apellido VARCHAR(100) NOT NULL,
+    dni VARCHAR(8) NOT NULL,
+    fecha_nacimiento DATE NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    peso DOUBLE NOT NULL,
+    activo INT NOT NULL,
+    fecha_registro DATETIME NOT NULL,
+    usuario_registro VARCHAR(100) NOT NULL,
+    fecha_actualizacion DATETIME NULL,
+    usuario_actualizacion VARCHAR(100) NULL,
+    PRIMARY KEY(id_usuario)
+);
+
+CREATE TABLE sd_conductor (
+    id_conductor INT NOT NULL AUTO_INCREMENT,
+    id_usuario INT NOT NULL,
+    fecha_venc_licencia VARCHAR(100) NOT NULL,
+    clave_digital VARCHAR(4) NOT NULL,
+    activo INT NOT NULL,
+    fecha_registro DATETIME NOT NULL,
+    usuario_registro VARCHAR(100) NOT NULL,
+    fecha_actualizacion DATETIME NULL,
+    usuario_actualizacion VARCHAR(100) NULL,
+    PRIMARY KEY(id_conductor),
+    CONSTRAINT con_usu_fk
+        FOREIGN KEY (id_usuario)
+        REFERENCES sd_usuario (id_usuario)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
+);
+
+CREATE TABLE sd_revisor (
+    id_revisor INT NOT NULL AUTO_INCREMENT,
+    id_usuario INT NOT NULL,
+    contrasena VARCHAR(100) NOT NULL,
+    activo INT NOT NULL,
+    fecha_registro DATETIME NOT NULL,
+    usuario_registro VARCHAR(100) NOT NULL,
+    fecha_actualizacion DATETIME NULL,
+    usuario_actualizacion VARCHAR(100) NULL,
+    PRIMARY KEY(id_revisor),
+    CONSTRAINT usu_rev_fk
+        FOREIGN KEY (id_usuario)
+        REFERENCES sd_usuario (id_usuario)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
+);
+
+CREATE TABLE sd_cliente (
+    id_cliente INT NOT NULL AUTO_INCREMENT,
+    razon_social VARCHAR(100) NOT NULL,
+    ruc VARCHAR(11) NOT NULL,
+    activo INT NOT NULL,
+    fecha_registro DATETIME NOT NULL,
+    usuario_registro VARCHAR(100) NOT NULL,
+    fecha_actualizacion DATETIME NULL,
+    usuario_actualizacion VARCHAR(100) NULL,
+    PRIMARY KEY(id_cliente)
+);
+
+CREATE TABLE sd_sede_cliente (
+    id_sede_cliente INT NOT NULL AUTO_INCREMENT,
+    id_cliente INT NOT NULL,
+    direccion VARCHAR(100) NOT NULL,
+    distrito VARCHAR(100) NOT NULL,
+    activo INT NOT NULL,
+    fecha_registro DATETIME NOT NULL,
+    usuario_registro VARCHAR(100) NOT NULL,
+    fecha_actualizacion DATETIME NULL,
+    usuario_actualizacion VARCHAR(100) NULL,
+    PRIMARY KEY(id_sede_cliente),
+    CONSTRAINT scl_cli_fk
+        FOREIGN KEY (id_cliente)
+        REFERENCES sd_cliente (id_cliente)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
+);
+
+CREATE TABLE sd_producto (
+    id_producto INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR (50) NOT NULL,
+    activo INT NOT NULL,
+    fecha_registro DATETIME NOT NULL,
+    usuario_registro VARCHAR(100) NOT NULL,
+    fecha_actualizacion DATETIME NULL,
+    usuario_actualizacion VARCHAR(100) NULL,
+    PRIMARY KEY(id_producto)
+);
+
+CREATE TABLE sd_marca (
+    id_marca INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR (50) NOT NULL,
+    activo INT NOT NULL,
+    fecha_registro DATETIME NOT NULL,
+    usuario_registro VARCHAR(100) NOT NULL,
+    fecha_actualizacion DATETIME NULL,
+    usuario_actualizacion VARCHAR(100) NULL,
+    PRIMARY KEY(id_marca)
+);
+
+CREATE TABLE sd_unidad (
+    id_unidad INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR (50) NOT NULL,
+    activo INT NOT NULL,
+    fecha_registro DATETIME NOT NULL,
+    usuario_registro VARCHAR(100) NOT NULL,
+    fecha_actualizacion DATETIME NULL,
+    usuario_actualizacion VARCHAR(100) NULL,
+    PRIMARY KEY(id_unidad)
+);
+
+CREATE TABLE sd_producto_venta (
+    id_producto_venta INT NOT NULL AUTO_INCREMENT,
+    id_producto INT NOT NULL,
+    id_marca INT NOT NULL,
+    id_unidad INT NOT NULL,
+    stock_actual INT NOT NULL,
+    activo INT NOT NULL,
+    fecha_registro DATETIME NOT NULL,
+    usuario_registro VARCHAR(100) NOT NULL,
+    fecha_actualizacion DATETIME NULL,
+    usuario_actualizacion VARCHAR(100) NULL,
+    PRIMARY KEY(id_producto_venta),
+    CONSTRAINT pvt_pro_fk
+        FOREIGN KEY (id_producto)
+        REFERENCES sd_producto (id_producto)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    CONSTRAINT pvt_mar_fk
+        FOREIGN KEY (id_marca)
+        REFERENCES sd_marca (id_marca)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    CONSTRAINT pvt_uni_fk
+        FOREIGN KEY (id_unidad)
+        REFERENCES sd_unidad (id_unidad)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
+);
+
+CREATE TABLE sd_tipo_vehiculo (
+    id_tipo_vehiculo INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(20) NOT NULL,
+    PRIMARY KEY(id_tipo_vehiculo)
+);
+
+CREATE TABLE sd_vehiculo (
+    id_vehiculo INT NOT NULL AUTO_INCREMENT,
+    id_tipo_vehiculo INT NOT NULL,
+    placa VARCHAR(10) NOT NULL,
+    largo DOUBLE NOT NULL,
+    ancho DOUBLE NOT NULL,
+    altura DOUBLE NOT NULL,
+    peso DOUBLE NOT NULL,
+    fecha_venc_circulacion DATETIME NOT NULL,
+    tiene_tarjeta_propiedad INT NOT NULL,
+    fecha_venc_soat DATETIME NULL,
+    activo INT NOT NULL,
+    fecha_registro DATETIME NOT NULL,
+    usuario_registro VARCHAR(100) NOT NULL,
+    fecha_actualizacion DATETIME NULL,
+    usuario_actualizacion VARCHAR(100) NULL,
+    PRIMARY KEY(id_vehiculo),
+    CONSTRAINT veh_tvh_fk
+        FOREIGN KEY (id_tipo_vehiculo)
+        REFERENCES sd_tipo_vehiculo (id_tipo_vehiculo)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
+);
+
+CREATE TABLE sd_estado_orden (
+    id_estado_orden INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(100) NOT NULL,
+    PRIMARY KEY(id_estado_orden)
+);
+
+CREATE TABLE sd_orden_recojo (
+    id_orden_recojo INT NOT NULL AUTO_INCREMENT,
+    id_conductor INT NOT NULL,
+    id_sede_cliente INT NOT NULL,
+    id_producto_venta INT NOT NULL,
+    id_estado_orden INT NOT NULL,
+    id_tracto INT NOT NULL,
+    id_carreta INT NOT NULL,
+    cantidad INT NOT NULL,
+    activo INT NOT NULL,
+    fecha_registro DATETIME NOT NULL,
+    usuario_registro VARCHAR(100) NOT NULL,
+    fecha_actualizacion DATETIME NULL,
+    usuario_actualizacion VARCHAR(100) NULL,
+    PRIMARY KEY(id_orden_recojo),
+    CONSTRAINT orc_con_fk
+        FOREIGN KEY (id_conductor)
+        REFERENCES sd_conductor (id_conductor)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    CONSTRAINT orc_pvt_fk
+        FOREIGN KEY (id_producto_venta)
+        REFERENCES sd_producto_venta (id_producto_venta)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    CONSTRAINT orc_eor_fk
+        FOREIGN KEY (id_estado_orden)
+        REFERENCES sd_estado_orden (id_estado_orden)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    CONSTRAINT orc_tra_fk
+        FOREIGN KEY (id_tracto)
+        REFERENCES sd_vehiculo (id_vehiculo)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    CONSTRAINT orc_car_fk
+        FOREIGN KEY (id_carreta)
+        REFERENCES sd_vehiculo (id_vehiculo)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
+);
+
+CREATE TABLE sd_planta (
+    id_planta INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(100) NOT NULL,
+    direccion VARCHAR(100) NOT NULL,
+    distrito VARCHAR(100) NOT NULL,
+    capacidad_maxima INT NOT NULL,
+    ubicacion_x1 DOUBLE NOT NULL,
+    ubicacion_x2 DOUBLE NOT NULL,
+    ubicacion_y1 DOUBLE NOT NULL,
+    ubicacion_y2 DOUBLE NOT NULL,
+    limite_inf_pesaje_antes INT NOT NULL,
+    limite_sup_pesaje_antes INT NOT NULL,
+    limite_inf_pesaje_despues INT NOT NULL,
+    limite_sup_pesaje_despues INT NOT NULL,
+    activo INT NOT NULL,
+    fecha_registro DATETIME NOT NULL,
+    usuario_registro VARCHAR(100) NOT NULL,
+    fecha_actualizacion DATETIME NULL,
+    usuario_actualizacion VARCHAR(100) NULL,
+    PRIMARY KEY(id_planta)
+);
+
+CREATE TABLE sd_punto_control (
+    id_punto_control INT NOT NULL AUTO_INCREMENT,
+    id_planta INT NOT NULL,
+    codigo VARCHAR (10) NOT NULL,
+    activo INT NOT NULL,
+    fecha_registro DATETIME NOT NULL,
+    usuario_registro VARCHAR(100) NOT NULL,
+    fecha_actualizacion DATETIME NULL,
+    usuario_actualizacion VARCHAR(100) NULL,
+    PRIMARY KEY(id_punto_control),
+    CONSTRAINT ptc_pla_fk
+        FOREIGN KEY (id_planta)
+        REFERENCES sd_planta (id_planta)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
+);
+
+CREATE TABLE sd_turno_revision (
+    id_turno_revision INT NOT NULL AUTO_INCREMENT,
+    id_revisor INT NOT NULL,
+    id_punto_control INT NOT NULL,
+    turno_dia INT NOT NULL,
+    hora_inicio DATETIME NOT NULL,
+    hora_fin DATETIME NULL,
+    es_aprobado INT NULL,
+    activo INT NOT NULL,
+    fecha_registro DATETIME NOT NULL,
+    usuario_registro VARCHAR(100) NOT NULL,
+    fecha_actualizacion DATETIME NULL,
+    usuario_actualizacion VARCHAR(100) NULL,
+    PRIMARY KEY(id_turno_revision),
+    CONSTRAINT trv_rev_fk
+        FOREIGN KEY (id_revisor)
+        REFERENCES sd_revisor (id_revisor)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    CONSTRAINT trv_pct_fk
+        FOREIGN KEY (id_punto_control)
+        REFERENCES sd_punto_control (id_punto_control)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
+);
+
+CREATE TABLE sd_incidencia (
+    id_incidencia INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(100) NOT NULL,
+    activo INT NOT NULL,
+    fecha_registro DATETIME NOT NULL,
+    usuario_registro VARCHAR(100) NOT NULL,
+    fecha_actualizacion DATETIME NULL,
+    usuario_actualizacion VARCHAR(100) NULL,
+    PRIMARY KEY(id_incidencia)
+);
+
+CREATE TABLE sd_turno_incidencia (
+    id_turno_incidencia INT NOT NULL AUTO_INCREMENT,
+    id_turno_revision INT NOT NULL,
+    id_incidencia INT NOT NULL,
+    activo INT NOT NULL,
+    fecha_registro DATETIME NOT NULL,
+    usuario_registro VARCHAR(100) NOT NULL,
+    fecha_actualizacion DATETIME NULL,
+    usuario_actualizacion VARCHAR(100) NULL,
+    PRIMARY KEY(id_turno_incidencia),
+    CONSTRAINT tin_tre_fk
+        FOREIGN KEY (id_turno_revision)
+        REFERENCES sd_turno_revision (id_turno_revision)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    CONSTRAINT tin_pct_fk
+        FOREIGN KEY (id_incidencia)
+        REFERENCES sd_incidencia (id_incidencia)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
+);
+
+CREATE TABLE sd_estado_despacho (
+    id_estado_despacho INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(100) NOT NULL,
+    PRIMARY KEY(id_estado_despacho)
+);
+
+CREATE TABLE sd_despacho (
+    id_despacho INT NOT NULL AUTO_INCREMENT,
+    id_planta INT NOT NULL,
+    id_orden_recojo INT NOT NULL,
+    id_estado_despacho INT NOT NULL,
+    id_turno_revision INT NOT NULL,
+    valor_pesaje_antes DOUBLE NULL,
+    valor_pesaje_despues DOUBLE NULL,
+    hora_inicio_carga DOUBLE NULL,
+    hora_fin_carga DOUBLE NULL,
+    activo INT NOT NULL,
+    fecha_registro DATETIME NOT NULL,
+    usuario_registro VARCHAR(100) NOT NULL,
+    fecha_actualizacion DATETIME NULL,
+    usuario_actualizacion VARCHAR(100) NULL,
+    PRIMARY KEY(id_despacho),
+    CONSTRAINT des_pla_fk
+        FOREIGN KEY (id_planta)
+        REFERENCES sd_planta (id_planta)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    CONSTRAINT des_orc_fk
+        FOREIGN KEY (id_orden_recojo)
+        REFERENCES sd_orden_recojo (id_orden_recojo)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    CONSTRAINT des_esd_fk
+        FOREIGN KEY (id_estado_despacho)
+        REFERENCES sd_estado_despacho (id_estado_despacho)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    CONSTRAINT des_trv_fk
+        FOREIGN KEY (id_turno_revision)
+        REFERENCES sd_turno_revision (id_turno_revision)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
+);
+
+CREATE TABLE sd_zona_balanza (
+    id_zona_balanza INT NOT NULL AUTO_INCREMENT,
+    id_planta INT NOT NULL,
+    codigo VARCHAR(10) NOT NULL,
+    qr_fisico VARCHAR(50) NOT NULL,
+    contrasena VARCHAR(50) NOT NULL,
+    activo INT NOT NULL,
+    fecha_registro DATETIME NOT NULL,
+    usuario_registro VARCHAR(100) NOT NULL,
+    fecha_actualizacion DATETIME NULL,
+    usuario_actualizacion VARCHAR(100) NULL,
+    PRIMARY KEY(id_zona_balanza),
+    CONSTRAINT zbl_pla_fk
+        FOREIGN KEY (id_planta)
+        REFERENCES sd_planta (id_planta)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
+);
+
+CREATE TABLE sd_tipo_pesaje (
+    id_tipo_pesaje INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(100) NOT NULL,
+    PRIMARY KEY(id_tipo_pesaje)
+);
+
+CREATE TABLE sd_cola_pesaje (
+    id_cola_pesaje INT NOT NULL AUTO_INCREMENT,
+    id_despacho INT NOT NULL,
+    id_zona_balanza INT NOT NULL,
+    id_tipo_pesaje INT NOT NULL,
+    posicion INT NOT NULL,
+    activo INT NOT NULL,
+    fecha_registro DATETIME NOT NULL,
+    usuario_registro VARCHAR(100) NOT NULL,
+    fecha_actualizacion DATETIME NULL,
+    usuario_actualizacion VARCHAR(100) NULL,
+    PRIMARY KEY(id_cola_pesaje),
+    CONSTRAINT cps_des_fk
+        FOREIGN KEY (id_despacho)
+        REFERENCES sd_despacho (id_despacho)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    CONSTRAINT cps_zbl_fk
+        FOREIGN KEY (id_zona_balanza)
+        REFERENCES sd_zona_balanza (id_zona_balanza)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    CONSTRAINT cps_tps_fk
+        FOREIGN KEY (id_tipo_pesaje)
+        REFERENCES sd_tipo_pesaje (id_tipo_pesaje)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
+);
+
+CREATE TABLE sd_canal_carga (
+    id_canal_carga INT NOT NULL AUTO_INCREMENT,
+    id_planta INT NOT NULL,
+    codigo VARCHAR(10) NOT NULL,
+    qr_fisico VARCHAR(50) NOT NULL,
+    activo INT NOT NULL,
+    fecha_registro DATETIME NOT NULL,
+    usuario_registro VARCHAR(100) NOT NULL,
+    fecha_actualizacion DATETIME NULL,
+    usuario_actualizacion VARCHAR(100) NULL,
+    PRIMARY KEY(id_canal_carga),
+    CONSTRAINT ccg_pla_fk
+        FOREIGN KEY (id_planta)
+        REFERENCES sd_planta (id_planta)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
+);
+
+CREATE TABLE sd_cola_canal (
+    id_cola_canal INT NOT NULL AUTO_INCREMENT,
+    id_despacho INT NOT NULL,
+    id_canal_carga INT NOT NULL,
+    posicion INT NOT NULL,
+    activo INT NOT NULL,
+    fecha_registro DATETIME NOT NULL,
+    usuario_registro VARCHAR(100) NOT NULL,
+    fecha_actualizacion DATETIME NULL,
+    usuario_actualizacion VARCHAR(100) NULL,
+    PRIMARY KEY(id_cola_canal),
+    CONSTRAINT ccn_des_fk
+        FOREIGN KEY (id_despacho)
+        REFERENCES sd_despacho (id_despacho)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    CONSTRAINT ccn_ccr_fk
+        FOREIGN KEY (id_canal_carga)
+        REFERENCES sd_canal_carga (id_canal_carga)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
+);
